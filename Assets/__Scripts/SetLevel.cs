22108782467a2188e61d834af44d6f0009f84052
Andrew Schaus <andrewschaus3@gmail.com>
2021-04-14 15:44:40 -0400

    Initial commit

---
+++ Assets/__Scripts/SetLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLevel : MonoBehaviour
+{
+    public void Start()
+    {
+        playerVariables.PV.shootingScore = 0;
+        playerVariables.PV.triviaScore = 0;
+        playerVariables.PV.Level = true;
+    }
+}
---
+++ Assets/__Scripts/playerVariables.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerVariables: MonoBehaviour
+{
+    public static playerVariables PV;
+
+    public int triviaScore;
+    public int shootingScore;
+    public int passingScore;
+    public float Speed;
+    public bool Jersey1 = false;
+    public bool Jersey2 = false;
+    public bool DRose = false;
+    public bool Level = false;
+    public int experiencePoints;
+
+    private void Awake()
+    {
+       if(PV == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            PV = this;
+        }
+            
+       else if(PV != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
+
